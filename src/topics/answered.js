b493efc7d67f224f3591dab9ef4f5e4a32a9e0e5
Jocelyn <jmorning@andrew.cmu.edu>
2024-02-25 17:42:58 -0500

    Added the visual of seeing question marked answered and gave the ability to answer questions to admin and instructors

--- src/topics/answered.js
+++ src/topics/answered.js
@@ -1,31 +1,13 @@
 
 'use strict';
 
-const async = require('async');
-const _ = require('lodash');
-
-const db = require('../database');
-const user = require('../user');
-const posts = require('../posts');
-const notifications = require('../notifications');
-const categories = require('../categories');
-const privileges = require('../privileges');
-const meta = require('../meta');
-const utils = require('../utils');
-const plugins = require('../plugins');
-
 module.exports = function (Topics) {
+    // This function sets the answered field to 1 to indicate that
+    // the question has been answered for all
     Topics.markAsAnsweredForAll = async function (tid) {
-        const cid = await Topics.getTopicField(tid, 'cid');
-        await db.set(`answered:cid:${cid}:tid:${tid}`, 'yes');
+        // const thisUser = user.getUserFields(uid, ['username', 'userslug']),
+        // const cid = await Topics.getTopicField(tid, 'cid');
+        // 1 means it has been answered, 0 means not answered
+        Topics.setTopicField(tid, 'answered', 1);
     };
-
-    /*
-    Topics.isAnsweredForAll = async function (tid) {
-        const cid = await Topics.getTopicField(tid, 'cid');
-        const val = await db.get(`answered:cid:${cid}:tid:${tid}`);
-        return (val === 'yes');
-    };
-    */
-    
 };
